24b044d174ca223eeb5f7b238e71982129b69579
Alex Jensen <alex@enurant.net>
2017-09-29 15:57:30 -0400

    knock knock blackbelt

--- knockKnock/blackbelt.py
+++ knockKnock/blackbelt.py
@@ -1,4 +1,5 @@
 # Samuel Jensen, Knock Knock Joke Blackbelt, 9/28/2017
+# Checks user input, gets frustrated when user doesn't go with the joke, unnecessary recursion
 
 # Get user's name
 name = input("Hi what's your name?")
@@ -6,32 +7,41 @@ name = input("Hi what's your name?")
 # Ask user if they want to hear a joke
 hearJoke = input("Nice to meet you " + name + ", would you like to hear a knock knock joke?")
 
-
 # Define asking function
 def askJoke(response):
     # If user says 'yes' continue joke
     if response == "yes":
         whoThere = input("Knock knock")
-        return whoThere
+
+        # Convert whoThere to lowercase in case user capitalized
+        whoThere = whoThere.lower()
+
+        # If user continues the joke, continue
+        if whoThere == "who's there" or whoThere == "who is there":
+            inquisition = input("The Spanish inquisition")
+
+            # Convert inquisition to lowercase in case user capitalized
+            inquisition = inquisition.lower()
+
+            # Check if user input continues the joke
+            if inquisition == "the spanish inquisition who" or inquisition == "the spanish inquisition? who":
+                print("No one expects the Spanish Inquisition!")
+            # If not, ragequit
+            else:
+                print("I don't think you really understand how these jokes work")
+        # Otherwise start again
+        else:
+            misunderstandJoke = input("Do you know how a knock knock joke works?")
+            askJoke(misunderstandJoke)
+
     # If not, commend them, and quit
     elif response == "no":
         print("Good, anyone with any sense of humor wouldn't want to hear a knock knock joke")
+        return "no"
     # If neither, chastise them for not being straightforward
     else:
-        misunderstand = input("Please answer yes or no. Would you like to hear a knock knock joke?")
-        askJoke(misunderstand)
+        misunderstandYesNo = input("Please answer yes or no. Would you like to hear a knock knock joke?")
+        askJoke(misunderstandYesNo)
 
-
-# Define who is there function
-def whoIs(response):
-    # If response is equal to the user input in lowercase, continue joke
-    if response.lower == "who's there" or "who is there":
-        inquisition = input("The Spanish inquisition")
-    # Otherwise start again
-    else:
-        misunderstand = input("Do you know how a knock knock joke works?")
-        askJoke(misunderstand)
-
-
-# Call asking function
-whoIs(askJoke(hearJoke))
+# Call function using user input
+askJoke(hearJoke)
