f8310e8f7be602a6fd24f2f29a2481db79ce0d3b
Alex Jensen <alex@endurant.net>
2017-11-24 13:46:24 -0500

    gameOfLife Grid

--- gameOfLife/model.py
+++ gameOfLife/model.py
@@ -1,14 +1,67 @@
-def scan(array, x, y):
+class Grid:
+    def __init__(self, height, width):
+        self.list = [[0 for i in range(height)] for j in range(width)]
+
+    def __get__(self, yPos, xPos):
+        if yPos >= 0:
+            if xPos >= 0:
+                return self.list[yPos][xPos]
+            else:
+                raise ValueError
+        else:
+            raise ValueError
+
+    def __set__(self, yPos, xPos, value):
+        self.list[yPos][xPos] = value
+
+    def __scan__(self, yPos, xPos):
+        alive = 0
+
+        try:
+            if self.__get__(yPos, (xPos + 1)) == 1:
+                alive += 1
+            else:
+                alive += 0
+        except IndexError:
+            pass
+
+        try:
+            if self.__get__(yPos, (xPos - 1)) == 1:
+                alive += 1
+            else:
+                alive += 0
+        except IndexError:
+            pass
+
+        return alive
+
+
+currentGen = Grid(1, 4)
+currentGen.__set__(0, 2, 1)
+currentGen.__set__(0, 3, 1)
+
+'''
+def scan(array, y, x):
     alive = 0
 
-    for i in range(-1, 1):
-        for j in range(-1, 1):
-            try:
-                if array[x+i][y+j] == 1:
-                    alive += 1
-                else:
-                    alive += 0
-            except IndexError:
-                alive += 0
+    try:
+        if array[(x + 1)] == 1:
+            alive += 1
+        else:
+            alive += 0
+    except IndexError:
+        pass
+
+    try:
+        if array[(x - 1)] == 1:
+            alive += 1
+        else:
+            alive += 0
+    except IndexError:
+        pass
 
     return alive
+
+
+print(currentGen[-1])
+'''
--- gameOfLife/tests/modelTest.py
+++ gameOfLife/tests/modelTest.py
@@ -1,15 +1,20 @@
 import unittest
 from model import *
 
-testGrid = [[0, 0, 0, 0, 1],
-            [0, 0, 0, 0, 0],
-            [0, 0, 0, 0, 1],
-            [0, 0, 0, 1, 1]]
-
 
 class TestModel(unittest.TestCase):
-    def test_dead(self):
-        self.assertEqual(scan(testGrid, 0, 0), 0)
+    def test_origin(self):
+        self.assertEqual(currentGen.__scan__(0, 0), 0)
+
+    def test_alive(self):
+        self.assertEqual(currentGen.__scan__(0, 2), 1)
+
+    def test_alive_right(self):
+        self.assertEqual(currentGen.__scan__(0, 1), 1)
+
+    def test_alive_left(self):
+        self.assertEqual(currentGen.__scan__(0, 3), 1)
+
 
 if __name__ == '__main__':
     unittest.main()
