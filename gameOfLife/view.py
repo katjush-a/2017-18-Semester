e6394b6f7e3913dad4a150543681c3b4ed4bba91
Alex Jensen <alex@endurant.net>
2017-11-28 17:36:44 -0500

    basic input box

--- gameOfLife/view.py
+++ gameOfLife/view.py
@@ -3,13 +3,39 @@ from tkinter import *
 
 class Prompt(Frame):
     def __init__(self):
-        super().__init__()
+        Frame.__init__(self)
         self.master.title("Initialize Grid")
+
+        header = Label(text="Input Grid Dimensions")
+        header.grid(row=0)
+
+        # Horizontal cells label
+        horizontal = Label(text="Horizontal Cells: ")
+        horizontal.grid(row=1)
+
+        # Horizontal cells textbox
+        self.hInput = Entry()
+        self.hInput.grid(row=1, column=1)
+
+        # Vertical cells label
+        vertical = Label(text="Vertical Cells:")
+        vertical.grid(row=2)
+
+        # Vertical cells textbox
+        self.vInput = Entry()
+        self.vInput.grid(row=2, column=1)
+
+        submit = Button(text="Submit", command=self.__getCoords__)
+        submit.grid(row=3)
+
+    def __getCoords__(self):
+        print(self.hInput)
+        print(self.vInput)
 
 
 def main():
     root = Tk()
-    root.geometry("300x250+300+300")
+    root.geometry("325x100+300+300")
     window = Prompt()
     root.mainloop()
 
